e1ec66cc947d21d5eebc58f1ff686e027c217905
coopergoldenholt <coopergoldenholt12@gmail.com>
2021-08-02 11:41:20 -0600

    first commi

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core"
+import { Request, Response } from 'express'
+
+export type MyContext = {
+    em: EntityManager<IDatabaseDriver<Connection>>;
+    // @ts-ignore
+    req: Request & { session: Express.Session };
+    res: Response;
+}
+
